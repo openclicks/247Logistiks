e7d2f35986b330ca3454c37a3ee678de1e48c5e2
openclicks <openclicks@tutanota.com>
2021-12-04 10:06:32 +0000

    modify tracking details

--- src/app/tracking/tracking.component.ts
+++ src/app/tracking/tracking.component.ts
@@ -27,10 +27,10 @@ export class TrackingComponent implements OnInit {
 
 
       this.events1 = [
-        {status: 'Ordered', date: '15/09/2021 10:30', icon: PrimeIcons.SHOPPING_CART, color: '#ad3559', image: 'game-controller.jpg', details: 'Payment confirmed. Package Ordered'},
-        {status: 'Processing', date: '15/09/2021 11:00', icon: PrimeIcons.COG, color: '#ad3559', details: 'Order confirmed. Preparing to ship package'},
-        {status: 'Shipped', date: '17/09/2021 16:15', icon: PrimeIcons.TABLE, color: '#ad3559', details: 'Package has left origin'},
-        {status: 'In-Transit', date: '20/09/2021 10:00', icon: PrimeIcons.SEND, color: '#ad3559', details: 'Arriving on October 15, 2021'}
+        {status: 'Ordered', date: '19/11/2021 10:30', icon: PrimeIcons.SHOPPING_CART, color: '#ad3559', image: 'game-controller.jpg', details: 'Payment confirmed. Package Ordered'},
+        {status: 'Processing', date: '19/11/2021 11:00', icon: PrimeIcons.COG, color: '#ad3559', details: 'Order confirmed. Preparing to ship package'},
+        {status: 'Shipped', date: '21/11/2021 16:15', icon: PrimeIcons.TABLE, color: '#ad3559', details: 'Package has left origin'},
+        {status: 'In-Transit', date: '24/11/2021 10:00', icon: PrimeIcons.SEND, color: '#ad3559', details: 'Arriving on December 15, 2021'}
         // {status: 'Delivered', date: '16/10/2020 10:00', icon: PrimeIcons.CHECK, color: '#607D8B'}
     ];
 
